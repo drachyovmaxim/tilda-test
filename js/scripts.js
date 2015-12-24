692d17b4d85782d56ac31c877d953b3c03de265c
Maxim <drachyov.maxim@gmail.com>
2015-12-24 19:34:01 +0300

    update

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// $('#myCarousel3450857').touchwipe({
+//   wipeLeft: function() {
+//     $('#myCarousel3450857').carousel('next');
+//   },
+//   wipeRight: function() { $('#myCarousel3450857').carousel('prev');},
+//   min_move_x: 0         
+// });
+
+$(document).ready(function(){
+  // $("#myCarousel3450857").touchwipe({
+  //    wipeLeft: function() { $('#myCarousel3450857').carousel('next'); },
+  //    wipeRight: function() { alert("right"); },
+  //    min_move_x: 20,
+  //    preventDefaultEvents: true
+  // });
+  $('#myCarousel3450857').bcSwipe({ threshold: 20 });
+});
