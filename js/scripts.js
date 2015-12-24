3d800840e80b08e88509741e3ae2a4ca42808662
Maxim <drachyov.maxim@gmail.com>
2015-12-25 00:00:09 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,27 @@
 //   min_move_x: 0         
 // });
 
-$(document).ready(function(){
-  // $("#myCarousel3450857").touchwipe({
-  //    wipeLeft: function() { $('#myCarousel3450857').carousel('next'); },
-  //    wipeRight: function() { alert("right"); },
-  //    min_move_x: 20,
-  //    preventDefaultEvents: true
-  // });
-  $('#myCarousel3450857').bcSwipe({ threshold: 20 });
+// $(document).ready(function(){
+//   // $("#myCarousel3450857").touchwipe({
+//   //    wipeLeft: function() { $('#myCarousel3450857').carousel('next'); },
+//   //    wipeRight: function() { alert("right"); },
+//   //    min_move_x: 20,
+//   //    preventDefaultEvents: true
+//   // });
+//   $('#myCarousel3450857').bcSwipe({ threshold: 20 });
+// });
+
+jQuery(document).ready(function($) {
+
+var myElement = document.getElementById('myCarousel3450857');
+var mc = new Hammer(myElement);
+
+mc.on("swipeleft", function(ev) {
+$('#myCarousel3450857').carousel('next');
+});
+
+mc.on("swiperight", function(ev) {
+$('#myCarousel3450857').carousel('prev');
+});
+
 });
