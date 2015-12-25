f4ca9f11c99c98537b95abaf7a77ea1b312fe037
Maxim <drachyov.maxim@gmail.com>
2015-12-25 16:45:38 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,3 @@
-// $('#myCarousel3450857').touchwipe({
-//   wipeLeft: function() {
-//     $('#myCarousel3450857').carousel('next');
-//   },
-//   wipeRight: function() { $('#myCarousel3450857').carousel('prev');},
-//   min_move_x: 0         
-// });
-
-// $(document).ready(function(){
-//   // $("#myCarousel3450857").touchwipe({
-//   //    wipeLeft: function() { $('#myCarousel3450857').carousel('next'); },
-//   //    wipeRight: function() { alert("right"); },
-//   //    min_move_x: 20,
-//   //    preventDefaultEvents: true
-//   // });
-//   $('#myCarousel3450857').bcSwipe({ threshold: 20 });
-// });
-
-// jQuery(document).ready(function($) {
-//   var myElement = document.getElementById('myCarousel3450857');
-//   var mc = new Hammer(myElement);
-//   mc.on("swipeleft", function(ev) {
-//     $('#myCarousel3450857').carousel('next');
-//   });
-//   mc.on("swiperight", function(ev) {
-//     $('#myCarousel3450857').carousel('prev');
-//   });
-// });
-
 $(document).ready(function() { 
   $(".carousel-inner").swipe( {
     swipeLeft:function(event, direction, distance, duration) {
@@ -35,8 +6,10 @@ $(document).ready(function() {
     swipeRight: function(event, direction, distance, duration) {
       $(this).parent().carousel('prev'); 
     },
-    threshold: 10,
+    threshold: 20,
     preventDefaultEvents: false,
     allowPageScroll: "vertical"
   });
 });
+
+
