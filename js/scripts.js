dfd153961575afc60007f913e06163b1308c4188
Maxim <drachyov.maxim@gmail.com>
2015-12-25 12:26:30 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -27,17 +27,14 @@
 //   });
 // });
 
-$(document).ready(function() {  
-  //Enable swiping...
+$(document).ready(function() { 
   $("#myCarousel3450857").swipe( {
-    //Generic swipe handler for all directions
     swipeLeft:function(event, direction, distance, duration, fingerCount) {
-      $(this).parent().carousel('prev'); 
-    },
-    swipeRight: function() {
       $(this).parent().carousel('next'); 
     },
-    //Default is 75px, set to 0 for demo so any distance triggers swipe
-    threshold: 30
+    swipeRight: function() {
+      $(this).parent().carousel('prev'); 
+    },
+    threshold: 40
   });
 });
