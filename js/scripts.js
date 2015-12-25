45d8e01b0cbbb005bc9d1cab6445075ae4055837
Maxim <drachyov.maxim@gmail.com>
2015-12-25 16:01:06 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@
 // });
 
 $(document).ready(function() { 
-  $("#myCarousel3459946").swipe( {
+  $("#myCarousel3459946, myCarousel3459919").swipe( {
     swipeLeft:function(event, direction, distance, duration, fingerCount) {
       $(this).parent().carousel('next'); 
     },
